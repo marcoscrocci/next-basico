c2157c56fe567c22109c0842adc3442787338c46
Marcos M. Crocci <marcoscrocci@gmail.com>
2021-07-11 13:45:23 -0300

    Curso de NextJS da Cod3r - Commit inicial

---
+++ src/pages/rotas/params.jsx
@@ -0,0 +1,19 @@
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+export default function Params() {
+    const router = useRouter()
+
+    //const id = router.query.id
+    //const nome = router.query.nome
+    const { id, nome } = router.query;
+
+    return (
+        <div>
+            <h1>Rotas Params: {id} e {nome}</h1>
+            <Link href="/rotas" passHref>
+                <button>Voltar</button>
+            </Link>
+        </div>
+    )
+}
